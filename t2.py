1bb55015c7fd282f0fab86fb41348fe61b1015f3
rodrigovaleretto <rodrigovaleretto@usp.br>
2021-06-21 20:12:51 -0300

    Iniciando código do T2

---
+++ t2.py
@@ -0,0 +1,13 @@
+import random
+
+
+class vertice:
+    x: None
+    y: None
+
+
+def generateKNN(v, k, seed=None):
+    # v é o número de vértices à serem gerados, k é o número de vizinhos que cada vértice terá
+    # e seed é a semente utilizada para a geração aleatória
+    random.seed(seed)
+    verticesArray = []
