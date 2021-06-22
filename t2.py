7e96e74f1cea171a74de3a925414d46769f76420
rodrigovaleretto <rodrigovaleretto@usp.br>
2021-06-22 14:19:49 -0300

    Plots adicionados

--- t2.py
+++ t2.py
@@ -1,5 +1,5 @@
 import random
-import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 
 
@@ -12,6 +12,7 @@ class vertice:
         self._y = y
         self._arestas = arestas
 
+    # getters da classe
     @property
     def x(self):
         return self._x
@@ -24,6 +25,7 @@ class vertice:
     def arestas(self):
         return self._arestas
 
+    # setters da classe
     @x.setter
     def x(self, x):
         self._x = x
@@ -36,8 +38,13 @@ class vertice:
     def arestas(self, arestas):
         self._arestas = arestas
 
+    # definição da string gerada pela classe
     def __str__(self):
         return '(' + str(self._x) + ', ' + str(self._y) + ')'
+
+    # sobrecarga do operador ==
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
 
 class aresta:
@@ -49,6 +56,7 @@ class aresta:
         self._v2 = v2
         self._d = d
 
+    # getters da classe
     @property
     def v1(self):
         return self._v1
@@ -61,6 +69,7 @@ class aresta:
     def d(self):
         return self._d
 
+    # setters da classe
     @v1.setter
     def v1(self, v1):
         self._v1 = v1
@@ -73,6 +82,7 @@ class aresta:
     def d(self, d):
         self._d = d
 
+    # definição da string gerada pela classe
     def __str__(self):
         return str(self._v1) + ', ' + str(self._v2) + ', ' + str(self._d)
 
@@ -116,6 +126,8 @@ def generateKNN(v, k, seed=None):
         for j in i.arestas:
             print(j)
 
+    return verticesArray, distMatrix
+
 
 '''
     for i in verticesArray:
@@ -130,4 +142,25 @@ def generateKNN(v, k, seed=None):
 '''
 
 
-generateKNN(10, 3, 1)
+verticesArray, distMatrix = generateKNN(10, 3, 1)
+
+xScatter = []
+yScatter = []
+
+
+fig, ax = plt.subplots()
+
+for i in verticesArray:
+    xScatter.append(i.x)
+    yScatter.append(i.y)
+    for j in i.arestas:
+        # print(j)
+        xData = [j.v1.x, j.v2.x]
+        yData = [j.v1.y, j.v2.y]
+        ax.plot(xData, yData)
+
+ax.scatter(xScatter, yScatter)
+ax.set_xlim(-0.5)
+ax.set_ylim(-0.5)
+ax.grid(True)
+plt.show()
